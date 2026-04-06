67e854c735dea53f6a1a6bb521c1619141381d6a
79875 <nakor447@gmail.com>
2026-04-07 00:09:08 +0300

    add new branch with Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("===== ЧАСТЬ 1: Животные =====\n");
 
-        // Создаём животных
+
         Dog bobik = new Dog("Бобик");
         Dog rex = new Dog("Рекс");
         Cat murka = new Cat("Мурка");
         Cat barsik = new Cat("Барсик");
         Cat tom = new Cat("Том");
 
-        // Проверяем бег и плавание
+
         bobik.run(150);
         bobik.run(600);
         bobik.swim(5);
@@ -28,13 +28,13 @@ public class Main {
 
         System.out.println("\n===== ЧАСТЬ 2: Коты и миска =====\n");
 
-        // Создаём миску с 30 ед. еды
+
         Bowl bowl = new Bowl(30);
 
-        // Массив котов
+
         Cat[] cats = {murka, barsik, tom};
 
-        // Коты пытаются поесть
+
         for (Cat cat : cats) {
             cat.eat(bowl, 15);
         }
@@ -44,11 +44,11 @@ public class Main {
             System.out.println(cat.name + ": " + (cat.isFull() ? "Сыт" : "Голоден"));
         }
 
-        // Добавляем еду в миску
+
         System.out.println("\n=== Добавляем еду в миску ===");
         bowl.addFood(20);
 
-        // Пусть голодный кот поест
+
         System.out.println("\n=== Том пробует поесть снова ===");
         tom.eat(bowl, 15);
 

da75eeba12cb91e3a7a759e4c775a8c319adec6b
79875 <nakor447@gmail.com>
2026-04-06 17:09:45 +0300

    add new branch with Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,38 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) {
+        System.out.println("===== ЗАДАНИЕ 1 и 2: Товары =====\n");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+
+        Product[] productsArray = new Product[5];
+
+
+        productsArray[0] = new Product("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 5599, true);
+        productsArray[1] = new Product("iPhone 16 Pro", "15.03.2025", "Apple Inc.", "USA", 8999, false);
+        productsArray[2] = new Product("Xiaomi 14", "10.01.2025", "Xiaomi Corp.", "China", 3499, true);
+        productsArray[3] = new Product("Google Pixel 9", "20.02.2025", "Google LLC", "USA", 4999, false);
+        productsArray[4] = new Product("OnePlus 12", "05.03.2025", "OnePlus", "China", 3999, true);
+
+
+        for (int i = 0; i < productsArray.length; i++) {
+            System.out.println("Товар №" + (i + 1));
+            productsArray[i].displayInfo();
         }
+
+        System.out.println("\n===== ЗАДАНИЕ 3: Аттракционы =====\n");
+
+
+        Park centralPark = new Park("Центральный парк");
+
+
+        Park.Attraction attraction1 = centralPark.new Attraction("Колесо обозрения", "10:00-22:00", 250);
+        Park.Attraction attraction2 = centralPark.new Attraction("Американские горки", "11:00-21:00", 400);
+        Park.Attraction attraction3 = centralPark.new Attraction("Автодром", "10:00-20:00", 200);
+
+
+        attraction1.displayInfo();
+        attraction2.displayInfo();
+        attraction3.displayInfo();
     }
 }

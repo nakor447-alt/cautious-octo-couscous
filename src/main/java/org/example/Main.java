13a4d6bbee08ef5f6cc5292884b6e4ad976c0082
79875 <nakor447@gmail.com>
2026-04-14 17:17:00 +0300

    add new branch with Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,106 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+class MyArrayDataException extends Exception {
+    private int row;
+    private int col;
+    private String wrongValue;
+
+    public MyArrayDataException(int row, int col, String wrongValue) {
+        super(String.format("Ошибка в ячейке [%d][%d]: значение '%s' не является числом", row, col, wrongValue));
+        this.row = row;
+        this.col = col;
+        this.wrongValue = wrongValue;
+    }
+
+    public int getRow() { return row; }
+    public int getCol() { return col; }
+    public String getWrongValue() { return wrongValue; }
+}
+
+public class Main {
+
+    public static int sumStringArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4) {
+            throw new MyArraySizeException("Ожидался массив 4x4, а получено строк: " + array.length);
         }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException(
+                        String.format("Ожидался массив 4x4, а строка %d имеет длину %d", i, array[i].length)
+                );
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    int value = Integer.parseInt(array[i][j]);
+                    sum += value;
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j, array[i][j]);
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("=== Тест 1: правильный массив ===");
+        String[][] correctArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+        try {
+            int sum = sumStringArray(correctArray);
+            System.out.println("Сумма элементов: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        System.out.println("\n=== Тест 2: неправильный размер (3x4) ===");
+        String[][] wrongSizeArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"}
+        };
+        try {
+            int sum = sumStringArray(wrongSizeArray);
+            System.out.println("Сумма элементов: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        System.out.println("\n=== Тест 3: некорректные данные ===");
+        String[][] wrongDataArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "a", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+        try {
+            int sum = sumStringArray(wrongDataArray);
+            System.out.println("Сумма элементов: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        System.out.println("\n=== Тест 4: ArrayIndexOutOfBoundsException ===");
+        int[] smallArray = new int[3];
+        try {
+            smallArray[5] = 100;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Поймано исключение: " + e);
+            System.out.println("Сообщение: " + e.getMessage());
+        }
+        System.out.println("Программа продолжает работу после обработки исключения.");
     }
 }

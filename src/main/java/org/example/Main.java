32a563792099239c1e9e013db2f2fa7c4ec7d9d4
79875 <nakor447@gmail.com>
2026-04-28 14:43:35 +0300

    add new branch with Triangle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        // Создаем фигуры
+
         Circle circle = new Circle(5.0, "Красный", "Черный");
         Rectangle rectangle = new Rectangle(4.0, 6.0, "Синий", "Желтый");
         Triangle triangle = new Triangle(3.0, 4.0, 5.0, "Зеленый", "Оранжевый");
 
-        // Выводим информацию о каждой фигуре
+
         circle.displayInfo();
         rectangle.displayInfo();
         triangle.displayInfo();
 
-        // Дополнительно: демонстрация полиморфизма
+
         System.out.println("\n=== Демонстрация полиморфизма ===");
         Figure[] figures = {circle, rectangle, triangle};
         for (Figure figure : figures) {
